712f54b8e8cba8dd29afa04a3a71b5ab72946acf
Daniel Dao <danieldkdao@gmail.com>
2025-08-11 19:46:01 -0700

    sixth commit

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const baseUrl = process.env.REACT_APP_API_URL
-console.log(baseUrl);
-
 const api = axios.create({
-    baseURL: baseUrl
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000"
 });
 
 export default api;
