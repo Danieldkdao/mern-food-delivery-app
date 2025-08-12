deb4dc2dbb3fc9328123d6dff4e48b45e749c15b
Daniel Dao <danieldkdao@gmail.com>
2025-08-11 20:37:40 -0700

    please work

--- backend/src/models/User/Cart/getOrders.ts
+++ backend/src/models/User/Cart/getOrders.ts
@@ -3,6 +3,7 @@ import UserModel from "../UserModel.js";
 const getOrders = async (userId: string) => {
     try {
         const user = await UserModel.findById(userId).populate("orders.order.id");
+        console.log(user?.orders);
         return {success: true, message: "Orders retrieved successfully!", orders: user?.orders};
     } catch (error) {
         console.error(error);
